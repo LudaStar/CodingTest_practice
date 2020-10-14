038b06319ced938f26231a777c2541f8e14a9473
LudaStar <LudaStar@github.com>
2020-10-14 20:55:25 +0900

    두개 뽑아서 더하기

--- codingTest/src/codingTest/Level1_1.java
+++ codingTest/src/codingTest/Level1_1.java
@@ -10,14 +10,14 @@ public class Level1_1 {
 		
 		//두 개 뽑아서 더하기, 정숫 배열 numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를 배열에 오름차순으로 return
 		
-		Solution sol = new Solution();
+		Solution1_1 sol = new Solution1_1();
 		int[] numbers = {2,1,3,4,1};
 		sol.solution(numbers);
 	}
 
 }
 
-class Solution {
+class Solution1_1 {
 	public int[] solution(int[] numbers) {
 		
 		ArrayList<Integer> list = new ArrayList<Integer>();
@@ -26,7 +26,7 @@ class Solution {
 			for(int j = i+1; j < numbers.length; j++) {
 				int sum = numbers[i] + numbers[j];
 				System.out.println("sum : " + sum + "  list.indexOf(sum) : "+list.indexOf(sum));
-				if(list.indexOf(sum) < 0) //indexOf - 지정된 객체의 위치를 반환, List의 첫 번째 요소부터 순방향으로 찾는다.
+				if(list.indexOf(sum) < 0) //indexOf - 지정된 객체의 위치를 반환 만약 없으면 -1을 반환, List의 첫 번째 요소부터 순방향으로 찾는다.
 					list.add(sum);
 			}
 		}
